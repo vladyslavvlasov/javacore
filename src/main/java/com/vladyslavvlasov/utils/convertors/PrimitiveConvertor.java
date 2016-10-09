696f52fbf312de4467ebaeb3d677a1b35923112a
Vladyslav Vlasov <vladyslavvlasov@gmail.com>
2016-10-09 13:13:34 +0300

    Format code ofLesson3Runner and Primitive converter/delete App.class

--- src/main/java/com/vladyslavvlasov/utils/convertors/PrimitiveConvertor.java
+++ src/main/java/com/vladyslavvlasov/utils/convertors/PrimitiveConvertor.java
@@ -5,21 +5,21 @@ package com.vladyslavvlasov.utils.convertors;
  */
 public class PrimitiveConvertor {
 
-    public char floatToChar(float float1){
-char char1 = (char) float1;
-        System.out.println ("Input float value is " + float1 +". Output char value is " + char1);
+    public char floatToChar(float float1) {
+        char char1 = (char) float1;
+        System.out.println("Input float value is " + float1 + ". Output char value is " + char1);
         return char1;
     }
 
-    public char intToChar (int int1){
-char char1 = (char)int1;
-        System.out.println ("Input int value is " + int1 +". Output char value is " + char1);
+    public char intToChar(int int1) {
+        char char1 = (char) int1;
+        System.out.println("Input int value is " + int1 + ". Output char value is " + char1);
         return char1;
     }
 
-    public int charToInt (char char1){
-int int1 = (int)char1;
-        System.out.println ("Input char value is " + (char)char1 +". Output int value is " + int1);
+    public int charToInt(char char1) {
+        int int1 = (int) char1;
+        System.out.println("Input char value is " + (char) char1 + ". Output int value is " + int1);
         return int1;
     }
 }
