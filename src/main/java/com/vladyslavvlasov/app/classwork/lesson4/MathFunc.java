41e350f8fa0db8147002ccb10af38daf51226d69
Vladyslav Vlasov <vladyslavvlasov@gmail.com>
2016-10-13 14:29:32 +0300

    Add App and Apptest to ignore

---
+++ src/main/java/com/vladyslavvlasov/app/classwork/lesson4/MathFunc.java
@@ -0,0 +1,9 @@
+package com.vladyslavvlasov.app.classwork.lesson4;
+
+/**
+ * Created by Murcielago on 12.10.2016.
+ */
+public class MathFunc {
+    public int multiply (int a, int b){return a * b;  }
+
+}
---
+++ src/test/java/com/vladyslavvlasov/app/classwork/lesson4/MathFuncTest.java
@@ -0,0 +1,16 @@
+package com.vladyslavvlasov.app.classwork.lesson4;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Murcielago on 12.10.2016.
+ */
+public class MathFuncTest {
+    @Test // JUnit framework annotation
+    public void multiplyTest() {
+        int a =100, b =2,expRes =200;
+        MathFunc math1 = new MathFunc();
+        //math1.multiply(1,2);
+        Assert.assertEquals(expRes, math1.multiply(a,b));
+    }
+}
